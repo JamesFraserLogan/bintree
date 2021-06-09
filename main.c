d1771d0bccdc0feb5643a03dbfe7b9a8e1069d3b
jamesfraserlogan <jamesfraserlogan@gmail.com>
2021-06-09 16:08:26 -0600

    fixed

--- main.c
+++ main.c
@@ -1,13 +1,5 @@
 #include "tree.h"
-#include <time.h>
 int main() {
-
-    clock_t start, end;
-    double cpu_time_used;
-
-    start = clock();
-
-
     Tree t0=NewTree();
     Tree t1=NewTree();
     Tree t2=NewTree();
@@ -20,19 +12,12 @@ int main() {
     SetTree(t3,3);
     SetTree(t4,4);
     SetTree(t5,5);
-
     BranchTree(t0,t1,t2);
     BranchTree(t1,t3,t4);
     BranchTree(t2,NULL,t5);
     PrintTree(t0,0);
     ReverseTree(t0);
     PrintTree(t0,0);
-
-
-
     DelBranches(t0);
-
-
-
     return 0;
 }
